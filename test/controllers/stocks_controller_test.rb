4cf7172581003235f36b1d860367d85d8a007ee9
jescolar10 <javierescolar10@gmail.com>
2017-05-19 09:08:58 +0000

    se añaden los test el clud9

--- test/controllers/stocks_controller_test.rb
+++ test/controllers/stocks_controller_test.rb
@@ -1,48 +1,11 @@
 require 'test_helper'
 
 class StocksControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @stock = stocks(:one)
+  test "ruta index" do
+      assert_routing '/stocks', controller: 'stocks', action: 'index'
   end
 
-  test "should get index" do
-    get stocks_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_stock_url
-    assert_response :success
-  end
-
-  test "should create stock" do
-    assert_difference('Stock.count') do
-      post stocks_url, params: { stock: { expiration_date: @stock.expiration_date, product_id: @stock.product_id, quantity: @stock.quantity } }
-    end
-
-    assert_redirected_to stock_url(Stock.last)
-  end
-
-  test "should show stock" do
-    get stock_url(@stock)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_stock_url(@stock)
-    assert_response :success
-  end
-
-  test "should update stock" do
-    patch stock_url(@stock), params: { stock: { expiration_date: @stock.expiration_date, product_id: @stock.product_id, quantity: @stock.quantity } }
-    assert_redirected_to stock_url(@stock)
-  end
-
-  test "should destroy stock" do
-    assert_difference('Stock.count', -1) do
-      delete stock_url(@stock)
-    end
-
-    assert_redirected_to stocks_url
+  test "ruta edit" do
+      assert_routing '/stocks/2/edit', controller: 'stocks', action: 'edit', id: '2'
   end
 end
