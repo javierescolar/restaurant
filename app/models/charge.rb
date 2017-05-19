d1f7ca3cd03b4b73791c1b0a3fd0d33a5a1101ae
jescolar10 <javierescolar10@gmail.com>
2017-05-19 09:51:50 +0000

    solucion fallo en la resta de stock

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -6,9 +6,13 @@ class Charge < ApplicationRecord
   def preparedDishYesNo
     self.prepared = (self.prepared) ? false : true
     
+    product_no_repeat = 0
     self.plate.dishes_products.each  do |product|
-      stock_selection = Stock.where('quantity >= ?',product.quantity).order('expiration_date').first
-      stock_selection.updateQuantityStock(product.quantity, self.prepared)
+      if product_no_repeat != product.id
+        product_no_repeat = product.id
+        stock_selection = Stock.where(product_id: product.id).where('quantity >= ?',product.quantity).order('expiration_date').first
+        stock_selection.updateQuantityStock(product.quantity, self.prepared)
+      end
     end
     
     self.save
